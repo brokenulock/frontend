7979caf17e40678456e0938e0bdb7fa4bd3d6975
fixmylifedesigns <ijd.irving@gmail.com>
2019-08-26 17:09:01 -0400

    nav

--- src/components/landingpage/navigation.js
+++ src/components/landingpage/navigation.js
@@ -3,6 +3,7 @@ import Logo from "../../bulfmlimg/navlogo.png";
 import { Link as Scroll } from "react-scroll";
 import TabletMenu from './material-ui/materialUI'
 
+
 export default function Navigation() {
   return (
     <div className="navigationContainer">
@@ -20,7 +21,13 @@ export default function Navigation() {
       </Scroll>
       <TabletMenu />
       <nav className="navigation">
-        <a href="https://reactlocation.netlify.com/" className="footerLink">Prototype Map</a>
+        <a href="https://reactlocation.netlify.com/" className="footerLink" style={{marginRight:'15px', borderBottom:"solid 2px"}}>Prototype Map</a>
+        <a href="https://documenter.getpostman.com/view/7133880/SVfMUAdJ?version=latest" className="footerLink" style={{marginRight:'15px', borderBottom:"solid 2px"}}>
+              Api Doc
+            </a>
+            <a href="https://github.com/brokenulock" className="footerLink" style={{marginRight:'15px', borderBottom:"solid 2px"}}>
+              Github Repo
+            </a>
       </nav>
     </div>
   );
